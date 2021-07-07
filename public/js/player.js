9246f8ead54539a03901e99433e115d8e118f5e9
HuynhThanhCanh <huynhthanhcanh.top@gmail.com>
2021-07-07 12:52:52 +0700

    Update

--- public/js/player.js
+++ public/js/player.js
@@ -2,19 +2,24 @@ var track = document.getElementById("track");
 
 var controlBtn = document.getElementById("play-pause");
 
+if (track.paused) {
+  controlBtn.className = "play";
+} else {
+  controlBtn.className = "pause";
+}
+
 function playPause() {
   if (track.paused) {
     track.play();
-    //controlBtn.textContent = "Pause";
     controlBtn.className = "pause";
   } else {
     track.pause();
-    //controlBtn.textContent = "Play";
     controlBtn.className = "play";
   }
 }
 
 controlBtn.addEventListener("click", playPause);
+
 track.addEventListener("ended", function () {
   controlBtn.className = "play";
 });
