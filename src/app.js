4b099f22236942e43a8656c5a9eaf44265ae47c4
Huỳnh Thanh Cảnh <macbookpro@Huynh-Thanh-Canh.local>
2021-11-24 17:46:28 +0700

    fix code to deploy on netlify

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const path = require("path");
 const app = express();
 const hbs = require("hbs");
 const projectDB = require("./db/project.json");
+const serverless = require("serverless-http");
+const router = express.Router();
 
 const publicDirectory = path.join(__dirname, "../public");
 const viewsDirectory = path.join(__dirname, "../public/template");
@@ -25,9 +27,17 @@ app.set("view engine", "hbs");
 
 app.set("views", viewsDirectory);
 
-app.get("/", function (request, response) {
+router.get("/", function (request, response) {
   response.render("index", { projectDB });
 });
 
-console.log("LISTENING PORT 3000");
-app.listen(process.env.PORT || 3000);
+router.get("/test", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("<h1>Hello from Express.js!</h1>");
+  res.end();
+});
+
+app.use("/.netlify/functions/server", router);
+module.exports.handler = serverless(app);
+// console.log("LISTENING PORT 3000");
+// app.listen(process.env.PORT || 3000);
