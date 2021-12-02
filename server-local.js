2b2a75f47939cf9ca68e669a5289d7f3c6a21178
Huỳnh Thanh Cảnh <macbookpro@Huynh-Thanh-Canh.local>
2021-12-02 14:18:27 +0700

    add env

--- server-local.js
+++ server-local.js
@@ -1,6 +1,9 @@
 "use strict";
 
+require("dotenv").config();
 const app = require("./src/server");
 
-console.log("LISTENING PORT 3000");
-app.listen(process.env.PORT || 3000);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT);
+console.log(`LISTENING PORT ${PORT}`);
